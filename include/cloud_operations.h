c09441ff882406ce74a39bcecd40a97b2fc1a977
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 22:28:04 +0530

    Create cloud_operations.h

---
+++ include/cloud_operations.h
@@ -0,0 +1,16 @@
+#ifndef CLOUD_OPERATIONS_H
+#define CLOUD_OPERATIONS_H
+
+// Function to upload file to cloud storage
+void upload_file();
+
+// Function to download file from cloud storage
+void download_file();
+
+// Function to delete file from cloud storage
+void delete_file();
+
+// Function to list all files in cloud storage
+void list_files();
+
+#endif
