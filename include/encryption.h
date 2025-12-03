589cf4e946b114db53e29e899b7666517339e1e2
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 20:17:43 +0530

    Update encryption.h

--- include/encryption.h
+++ include/encryption.h
@@ -1,1 +1,7 @@
-hl
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+void encrypt_file(const char *input_path, const char *output_path, int key);
+void decrypt_file(const char *input_path, const char *output_path, int key);
+
+#endif // ENCRYPTION_H
