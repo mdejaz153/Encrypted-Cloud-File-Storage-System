17690947f8287da156028fd2fd9279754f4c9af1
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 22:16:15 +0530

    Update encryption.h

--- include/encryption.h
+++ include/encryption.h
@@ -1,7 +1,10 @@
 #ifndef ENCRYPTION_H
 #define ENCRYPTION_H
 
-void encrypt_file(const char *input_path, const char *output_path, int key);
-void decrypt_file(const char *input_path, const char *output_path, int key);
+// Function to encrypt file content using XOR cipher
+void xor_encrypt(char *data, int length, char *key);
+
+// Function to decrypt file content using XOR cipher
+void xor_decrypt(char *data, int length, char *key);
 
 #endif
