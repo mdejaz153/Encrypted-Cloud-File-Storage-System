dfac6ebaacd7b63cb65347e127fa8ea5772d46e8
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 22:16:49 +0530

    Update cloud.h

--- include/cloud.h
+++ include/cloud.h
@@ -1,15 +1,16 @@
-#ifndef CLOUD_H
-#define CLOUD_H
+#ifndef CLOUD_OPERATIONS_H
+#define CLOUD_OPERATIONS_H
 
-#define CLOUD_FOLDER "cloud_storage/"
-#define META_FILE "cloud_storage/metadata.txt"
+// Function to upload file to cloud storage
+void upload_file();
 
-void build_filename(int id, int version, char *out);
-int generate_file_id();
-int get_next_version(int id);
-void save_metadata(int id, const char *name, int version);
+// Function to download file from cloud storage
+void download_file();
+
+// Function to delete file from cloud storage
+void delete_file();
+
+// Function to list all files in cloud storage
 void list_files();
-int file_exists_cloud(int id, int version);
-int delete_file_cloud(int id, int version);
 
 #endif
