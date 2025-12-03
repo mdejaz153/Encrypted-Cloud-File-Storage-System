343266bb6fc9529d09cd67186224fb0451305c56
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 20:28:59 +0530

    Update cloud.h

--- include/cloud.h
+++ include/cloud.h
@@ -1,32 +1,15 @@
 #ifndef CLOUD_H
 #define CLOUD_H
 
-#include <stddef.h>
+#define CLOUD_FOLDER "cloud_storage/"
+#define META_FILE "cloud_storage/metadata.txt"
 
-#define CLOUD_DIR "cloud_storage"
-#define METADATA_FILE "cloud_storage/metadata.txt"
-#define MAX_FILENAME 256
+void build_filename(int id, int version, char *out);
+int generate_file_id();
+int get_next_version(int id);
+void save_metadata(int id, const char *name, int version);
+void list_files();
+int file_exists_cloud(int id, int version);
+int delete_file_cloud(int id, int version);
 
-typedef struct {
-    int file_id;
-    char original_name[MAX_FILENAME];
-    int version;
-} FileRecord;
-
-// Cloud initialization
-int initialize_cloud(void);
-
-// File ID and version handling
-int generate_file_id(void);
-int get_next_version(int file_id, char *original_name_out, size_t name_size);
-
-// Metadata handling
-int save_file_record(int file_id, const char *original_name, int version);
-
-// Utility functions
-void build_encrypted_filename(int file_id, int version, char *out, size_t size);
-int encrypted_file_exists(int file_id, int version);
-int delete_encrypted_file(int file_id, int version);
-void list_files(void);
-
-#endif // CLOUD_H
+#endif
