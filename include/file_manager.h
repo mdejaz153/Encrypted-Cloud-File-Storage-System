511e85f4f2dcabb66acfdd482bd80a9181ad0ee6
mdejaz153 <mdejazms365@gmail.com>
2025-12-03 22:50:33 +0530

    Update file_manager.h

--- include/file_manager.h
+++ include/file_manager.h
@@ -5,12 +5,15 @@
 void generate_file_id(char *file_id);
 
 // Function to add entry in metadata file
-void add_metadata(char *file_id, char *original_name, int version);
+void add_metadata(char *file_id, char *original_name, int version, char *key_hash);
 
 // Function to search file in metadata
-int search_metadata(char *file_id, char *encrypted_filename);
+int search_metadata(char *file_id, char *encrypted_filename, char *stored_key_hash);
 
 // Function to delete entry from metadata
 void delete_metadata(char *file_id);
+
+// Function to generate simple hash of key
+unsigned int generate_key_hash(char *key);
 
 #endif
